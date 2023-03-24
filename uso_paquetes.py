5771c1e1316600b848851e8c24f071b04ab3da52
kevinarauz <kecin222294@gmail.com>
2023-03-24 11:28:04 -0500

    Configurar entornos virtuales

--- uso_paquetes.py
+++ uso_paquetes.py
@@ -1,3 +1,4 @@
+#from calculos.funciones_matematicos import *
 from calculos.funciones_matematicos import suma, resta
 from calculos.basicos.multiplicacion_division import multiplicacion, division
 
@@ -13,3 +14,20 @@ multiplicacion(7, 3)
 #py .\setup.py sdist  #carpeta raiz proyecto
 #pip install .\paquetecalculo-1.0.tar.gz #en la carpeta dist
 #pip uninstall paquetecalculo  #Borrar paquetes que instale
+#----
+##pip install requests==2.28.2 #instalar libreria con version
+#---Entorno virtual
+#py -m venv venv #genera entorno virtual
+#cd .\venv\Scripts\
+#.\activate #activar entorno virtual
+#pip freeze
+#pip install #instalar en el script del entorno virtual
+#pip freeze > requirements.txt #va en la raiz del proyecto
+
+#crear el requerimiento
+#py -m venv venv #genera entorno virtual
+#cd .\venv\Script\
+#.\activate #dentro de script
+#pip freeze
+#pip install -r .\requirements.txt
+
